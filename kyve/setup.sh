9fd4aa9ff61653f7a017977fc1a0478b38ec04d8
MahersRocket Co-Pilot <kebin70@gmail.com>
2021-12-24 16:49:00 -0800

    l/g

--- kyve/setup.sh
+++ kyve/setup.sh
@@ -7,6 +7,6 @@ do
     POOL=$(echo $jsonData | jq -r .pool)
     RUNTIME=$(echo $jsonData | jq -r .runtime)
     echo $NAME
-    echo ./kyve-$RUNTIME-linux --pool $POOL --private-key `cat private.key` --keyfile arweave.json --name $NICK_$NAME --stake $STAKE > validate_$NAME.sh
+    echo ./kyve-$RUNTIME-linux --pool $POOL --private-key \`cat private.key\` --keyfile arweave.json --name $NICK_$NAME --stake $STAKE > validate_$NAME.sh
     chmod +x validate_$NAME.sh
 done
